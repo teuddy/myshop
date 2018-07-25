9b5c36a5012db39366e306bb5b41595b5b181f29
Owner <macowner@Owners-MacBook-Pro.local>
2018-07-24 21:20:16 -0500

    3

--- src/main/java/com/example/demo/controller/Usercontroller.java
+++ src/main/java/com/example/demo/controller/Usercontroller.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Usercontroller {
+
+    @GetMapping
+    public String hello(){
+        return " hello people ";
+    }
+
+    @PostMapping
+    public void hey(){
+        new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
 }
